32a25c0f800439549249058540d45cf1d629d3a4
Lukas Meinschad <lukas.meinschad@gmail.com>
2025-12-19 22:46:04 +0100

    Mapping

---
+++ Theory_Outline.md
@@ -0,0 +1,5 @@
+# Optimization Problem
+
+We consider the optimzation problem where the total energy of the molecule is minimized with respect to the positions of the nuclei. We can treet nuclei as point particles and consider the minimization function as $E(x)$ where $x$ is the vector of the positions of the nuclei in 3D space.
+
+We want to determine $\nabla_x E(x) = 0$ as a stationary point of this optimization problem.
---
+++ main.py
@@ -0,0 +1,37 @@
+import numpy as np
+from qiskit_nature.units import DistanceUnit
+from qiskit_nature.second_q.drivers import PySCFDriver
+from qiskit_nature.second_q.mappers import JordanWignerMapper
+from qiskit_nature.second_q.mappers import ParityMapper
+
+
+def parse_xyz_to_pyscf_driver(filepath, basis_set="sto3g"):
+    """  
+    Parses the molecule xyz file to the PySCFDriver object of qiskit-nature
+
+    atom="H 0 0 0; H 0 0 0.735" etc
+    """
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+        num_atoms = int(lines[0].strip())
+        atom_lines = lines[2:2 + num_atoms]
+        atom_string = "; ".join(line.strip() for line in atom_lines)
+    driver = PySCFDriver(atom=atom_string, basis=basis_set, unit=DistanceUnit.ANGSTROM,charge=0, spin=0)
+    return driver
+
+
+if __name__ == "__main__":
+    file_path = "/Users/lukas/Desktop/vu_quantum_computing/test_molecules/h2.xyz"
+    molecule = parse_xyz_to_pyscf_driver(file_path, basis_set="sto3g")
+     
+    # Obtain the electronic structure Hamiltonian of the molecule
+    es_problem = molecule.run()
+    fermionic_operator = es_problem.hamiltonian.second_q_op()
+
+    # Map the fermionic operator to a qubit operator using Jordan-Wigner mapping 
+    mapper = JordanWignerMapper()
+    qubit_jw_op = mapper.map(fermionic_operator)
+ 
+    # Use Parity Mapping
+    mapper = ParityMapper()
+    qubit_p_op = mapper.map(fermionic_operator)
